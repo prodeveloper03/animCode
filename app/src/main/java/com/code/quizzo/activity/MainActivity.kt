2780b00f730b0a49a8e022598f4d42f3ab4f52cd
sounak saha <sounaksaha100@gmail.com>
2025-08-09 00:05:16 +0530

    initial commit

---
+++ app/src/main/java/com/code/quizzo/activity/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.code.quizzo.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.code.quizzo.R
+
+
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.os.Build
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition { false }
+        setTheme(R.style.Theme_Quizzo)
+        super.onCreate(savedInstanceState)
+        hideSystemBars()
+
+        setContent {
+
+        }
+    }
+
+    private fun hideSystemBars() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.let {
+                it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        }
+    }
+}
+
+
